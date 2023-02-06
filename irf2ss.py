fd585f12de49dfa71341e7a5a275c554d11cb97e
Hydronautica <96926753+Hydronautica@users.noreply.github.com>
2023-02-05 22:08:36 -0500

    Update irf2ss.py

--- irf2ss.py
+++ irf2ss.py
@@ -1,15 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.signal import lsim2
 from scipy.optimize import minimize
 
-def fit_state_space_model(impulse_response, time):
+def fit_state_space_model(impulse_response, time, n):
     # Define the state-space model
     def state_space_model(params, input_signal, time):
-        A = np.array([[params[0], params[1]], [params[2], params[3]]])
-        B = np.array([params[4], params[5]])
-        C = np.array([params[6], params[7]])
-        D = params[8]
-        x0 = np.array([0, 0])
+        A = np.zeros((n, n))
+        for i in range(n):
+            for j in range(n):
+                A[i, j] = params[i * n + j]
+        B = params[n ** 2 : n ** 2 + n].reshape(n, 1)
+        C = params[n ** 2 + n : n ** 2 + n + n].reshape(1, n)
+        D = params[-1]
+        x0 = np.zeros((n, 1))
 
         # Simulate the state-space model
         _, y, _ = lsim2(A, B, C, D, input_signal, time, X0=x0)
@@ -22,18 +26,34 @@ def fit_state_space_model(impulse_response, time):
         return np.sum(difference ** 2)
     
     # Define the initial parameters for the optimization
-    params0 = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    params0 = np.ones(n ** 2 + 2 * n + 1)
 
     # Fit the state-space model to the impulse response
     result = minimize(objective_function, params0, args=(impulse_response, time, impulse_response))
     
-    return result.x
+    # Extract the state-space matrices from the optimized parameters
+    A = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            A[i, j] = result.x[i * n + j]
+    B = result.x[n ** 2 : n ** 2 + n].reshape(n, 1)
+    C = result.x[n ** 2 + n : n ** 2 + n + n].reshape(1, n)
+    D = result.x[-1]
+    
+    return A, B, C, D
 
 # Example input data
 impulse_response = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 time = np.arange(-2, 8)
+n = 2
 
 # Fit the state-space model to the impulse response
-params = fit_state_space_model(impulse_response, time)
+A, B, C, D = fit_state_space_model(impulse_response, time, n)
 
-print("Fitted state-space model parameters:", params)
+# Simulate the state-space model with an impulse input
+input_signal = np.zeros(len(time))
+input_signal[0] = 1
+_, model_response, _ = lsim2(A, B, C, D, input_signal, time)
+
+# Plot the original
+plt.plot
